7e1a104084804e4f1660648badfff0cdee07f330
Pantae <pantae35872@gmail.com>
2025-07-09 21:30:41 +0700

    Clean up

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,27 @@
-use rppal::gpio::Gpio;
+use tauri::AppHandle;
+#[cfg(feature = "rpi")]
+use {
+    rppal::gpio::{Gpio, OutputPin},
+    tauri::Manager,
+    tokio::sync::Mutex,
+};
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[cfg(feature = "rpi")]
+struct AppGpioPins {
+    gpio_23: Mutex<OutputPin>,
+}
+
+#[cfg(feature = "rpi")]
 #[tauri::command]
-fn greet(name: &str) -> String {
-    let mut pin = Gpio::new()
-        .expect("Failed to get gpio")
-        .get(23)
-        .unwrap()
-        .into_output();
-    pin.toggle();
-    format!("Hello, {name}! You've been greeted from Rust!")
+async fn toggle_gpio_23(app: AppHandle) {
+    let gpio_pins = app.state::<AppGpioPins>();
+    gpio_pins.gpio_23.lock().await.toggle();
+}
+
+#[cfg(not(feature = "rpi"))]
+#[tauri::command]
+async fn toggle_gpio_23(_: AppHandle) {
+    print!("GPIO Pin 23 toggle");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -17,10 +29,23 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
-            let _handle = app.handle().clone();
+            #[cfg(feature = "rpi")]
+            {
+                app.manage(AppGpioPins {
+                    gpio_23: Gpio::new()
+                        .expect("Failed to get gpio")
+                        .get(23)
+                        .unwrap()
+                        .into_output()
+                        .into(),
+                });
+            }
+
+            #[cfg(not(feature = "rpi"))]
+            let _ = app.handle(); // suppress unused warning
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![toggle_gpio_23])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
