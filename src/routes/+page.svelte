bf20fa6ecea207d3d4305564c87d75e3f4822760
Pantae <pantae35872@gmail.com>
2025-07-11 19:51:25 +0700

    Inventory ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import { invoke } from "@tauri-apps/api/core";
+    import Item from "./Item.svelte";
+    import "../style.css";
 
     async function toggleGpio23(event: Event) {
         await invoke("toggle_gpio_23");
@@ -9,47 +11,58 @@
     }
 </script>
 
-<main>
-    <h1>Welcome to Tauri + Svelte</h1>
-    <button class="button" onclick={toggleGpio23}>Toggle GPIO 23</button>
-    <button class="button" onclick={exit}>Exit</button>
+<main class="container">
+    <h1>Inventory</h1>
+    <div class="items-container">
+        <Item item_name="Resistor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+        <Item item_name="Motor" item_amount={20} />
+    </div>
+    <!-- <button class="button" onclick={toggleGpio23}>Toggle GPIO 23</button> 
+    <button class="button" onclick={exit}>Exit</button> -->
 </main>
 
 <style>
-    :root {
-        --fg-color-dark: rgba(255, 255, 255, 0.87);
-        --fg-color-2-dark: rgba(200, 200, 200, 1);
-        --bg-color-dark: #242424;
-        --bg-color-2-dark: #3a3a3a;
-        --bg-color-3-dark: #1a1a1a;
-        --bg-color-4-dark: #161616;
-
-        font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-        font-size: 16px;
-        line-height: 24px;
-        font-weight: 400;
-
-        color: var(--fg-color-2-dark);
-        background-color: var(--bg-color-3-dark);
-
-        font-synthesis: none;
-        text-rendering: optimizeLegibility;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        -webkit-text-size-adjust: 100%;
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1em;
     }
 
-    .button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        color: var(--fg-color-2-dark);
-        background-color: var(--bg-color-4-dark);
-        border-color: var(--bg-color-2-dark);
-        transition: border-color 0.25s;
-        box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+    .items-container {
+        display: flex;
+        gap: 1em;
+        flex-wrap: wrap;
+
+        align-items: center;
+        justify-content: start;
+
+        max-width: 80%;
     }
 </style>
---
+++ src/routes/Item.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    let {
+        item_name,
+        item_amount,
+    }: { item_name?: string; item_amount?: number } = $props();
+</script>
+
+<div class="item-container">
+    <span class="item-info">{item_name}</span>
+    <button class="button">Request</button>
+
+    <div class="item-amount">has {item_amount} left</div>
+</div>
+
+<style>
+    .item-container {
+        display: flex;
+        gap: 1em;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+
+        min-width: 15vh;
+        min-height: 12vh;
+        background-color: var(--bg-color-3);
+        padding: 10px;
+        border: 1px solid var(--bg-color-2);
+        border-radius: 0.5rem;
+        box-shadow: 0 5px 5px var(--bg-color-2);
+    }
+
+    .item-info {
+        text-align: center;
+        font-size: 1.4rem;
+    }
+
+    .item-amount {
+        position: absolute;
+        right: 0.4em;
+        bottom: 0.2em;
+    }
+</style>
