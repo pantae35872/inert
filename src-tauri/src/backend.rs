2527c4fe7d05affff6fb68761821e6c7cb3a1d0e
Pantae <pantae35872@gmail.com>
2025-08-08 01:36:27 +0700

    Position tune

--- src-tauri/src/backend.rs
+++ src-tauri/src/backend.rs
@@ -105,7 +105,7 @@ impl<'a, M: MotorBackend, S: LimitSwitchBackend> ProtectedMotor<'a, M, S> {
             return Ok(());
         }
 
-        const BLOCK_TURN: f32 = 0.25;
+        const BLOCK_TURN: f32 = 0.1;
 
         let rotation_need = MotorRotation {
             turns: BLOCK_TURN * amount as f32,
